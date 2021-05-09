3a062fb05729621d60d7b961f86afdeff9f8043c
AliEl-Hallak <alielhallak1234@gmail.com>
2021-05-10 01:12:24 +0300

    dördüncü commit

--- login.php
+++ login.php
@@ -10,13 +10,13 @@
     }
     elseif ($kulancı=="g201210559@sakarya.edu.tr")
     { 
-        echo " <script>location.replace('proja.html')
+        echo " <script>location.replace('login.html')
         alert('mail adrersi bulunmaktadır yalnız şiferinizi yanlıştır')  </script>";
         
     }
     else 
     { 
-        echo " <script>location.replace('proja.html')
+        echo " <script>location.replace('login.html')
         alert('mail adresi bulunmamaktadır')  </script>";
         
     }
